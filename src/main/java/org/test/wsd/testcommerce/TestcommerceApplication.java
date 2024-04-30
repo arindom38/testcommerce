9f66409cb8342199be8fe999ae110d91817e8528
ujjal38 <kundu.vivasoft@bkash.com>
2024-05-01 01:26:43 +0600

    Initialize project

---
+++ src/main/java/org/test/wsd/testcommerce/TestcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.test.wsd.testcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/test/wsd/testcommerce/TestcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.wsd.testcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
